9b0c0a8be2fcc4daab5c8e0c6f39066f0d75309b
M4recki <111280515+M4recki@users.noreply.github.com>
2023-12-02 17:37:05 +0100

    Added a footer with links to my social media as well as the current year.

--- project/python/routes.py
+++ project/python/routes.py
@@ -1,10 +1,21 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
+from datetime import datetime
 
 router = APIRouter()
-templates = Jinja2Templates(directory=Path(__file__).parent.parent / "templates")
+
+
+def current_year(request: Request):
+    return {"current_year": datetime.now().year}
+
+
+templates = Jinja2Templates(
+    directory=Path(__file__).parent.parent / "templates",
+    context_processors=[current_year]
+)
+
 
 @router.get("/")
 def root(request: Request):
-   return templates.TemplateResponse("main_page.html", {"request": request})
+    return templates.TemplateResponse("main_page.html", {"request": request})
