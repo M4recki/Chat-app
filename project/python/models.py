be9cb4fe9e58cb29fc4d65273a47902f8414c502
M4recki <111280515+M4recki@users.noreply.github.com>
2023-12-10 15:58:10 +0100

    The ability to add an account to the database has been added and the database structure has been improved.

--- project/python/models.py
+++ project/python/models.py
@@ -4,45 +4,55 @@ from sqlalchemy.ext.declarative import declarative_base
 from database import Base, engine
 
 
-
 # User table
 
+
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
+    surname = Column(String(100), nullable=False)
     email = Column(String(100), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
     created_at = Column(DateTime, nullable=False)
-    
+
+
 # Messages table
 
+
 class Message(Base):
-    __tablename__ = 'messages'
+    __tablename__ = "messages"
     id = Column(Integer, primary_key=True)
     content = Column(String(500), nullable=False)
     created_at = Column(DateTime, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     user = relationship("User", backref="messages")
+
 
 # Group table
 
+
 class Group(Base):
-    __tablename__ = 'groups'
+    __tablename__ = "groups"
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     created_at = Column(DateTime, nullable=False)
-    users = relationship("User", secondary="group_users", backref="groups")
+    users = relationship("User", secondary="group_users", backref="user_groups")
+
 
 # Group_users table
 
+
 class GroupUser(Base):
-    __tablename__ = 'group_users'
+    __tablename__ = "group_users"
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    group_id = Column(Integer, ForeignKey('groups.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    group_id = Column(Integer, ForeignKey("groups.id"), nullable=False)
     created_at = Column(DateTime, nullable=False)
     user = relationship("User", backref="group_users")
-    group = relationship("Group", backref="group_users")
-    
-User.groups = relationship("Group", secondary="group_users", backref="users")
+    group = relationship(
+        "Group", backref="group_users", overlaps="groups,user_groups,users"
+    )
+
+
+User.groups = relationship("Group", secondary="group_users", back_populates="users")
--- project/python/routes.py
+++ project/python/routes.py
@@ -1,8 +1,17 @@
 from fastapi import APIRouter, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
+from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import URLSafeTimedSerializer as Serializer
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
+from database import SessionLocal
+from email.message import EmailMessage
+import ssl
+import smtplib
+from os import environ
+from models import User
+
 
 router = APIRouter()
 
@@ -28,7 +37,7 @@ def root(request: Request):
 
 
 @router.get("/sign_up", name="sign_up")
-async def sign_up_page(request: Request):
+def sign_up_page(request: Request):
     return templates.TemplateResponse("sign_up.html", {"request": request})
 
 
@@ -42,29 +51,121 @@ async def sign_up_data(
     confirm_password: str = Form(...),
     terms_conditions: bool = Form(...),
 ):
-    redirect_url = request.url_for("root")
-    return RedirectResponse(redirect_url, status_code=303)
+    db = SessionLocal()
+    user = db.query(User).filter(User.email == email).first()
+
+    if user:
+        return templates.TemplateResponse(
+            "sign_up.html",
+            {"request": request, "error": "User with this email already exists"},
+        )
+
+    if len(email) < 8:
+        return templates.TemplateResponse(
+            "sign_up.html",
+            {
+                "request": request,
+                "error": "Password must be at least 8 characters long",
+            },
+        )
+
+    if not terms_conditions:
+        return templates.TemplateResponse(
+            "sign_up.html",
+            {"request": request, "error": "You must accept the terms and conditions"},
+        )
+
+    if email.find("@") == -1:
+        return templates.TemplateResponse(
+            "sign_up.html", {"request": request, "error": "Invalid email"}
+        )
+
+    if password != confirm_password:
+        return templates.TemplateResponse(
+            "sign_up.html", {"request": request, "error": "Passwords do not match"}
+        )
+
+    hashed_password = generate_password_hash(
+        password, method="pbkdf2:sha256", salt_length=6
+    )
+    new_user = User(
+        name=name,
+        surname=surname,
+        email=email,
+        password=hashed_password,
+        created_at=datetime.now(),
+    )
+    db.add(new_user)
+    db.commit()
+
+    SECRET_KEY = environ.get("Secret_key_chat")
+    s = Serializer(SECRET_KEY)
+    token = s.dumps({"user_id": new_user.id}, expires_in=timedelta(hours=1))
+
+    response = RedirectResponse(request.url_for("root"), status_code=303)
+    response.set_cookie(key="access_token", value=token, httponly=True)
+
+    return response
 
 
 # Login
 
 
 @router.get("/login", name="login")
-async def login_page(request: Request):
+def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
 
 @router.post("/login")
-async def login_data(request: Request, email: str = Form(...), password: str = Form(...)):
-    redirect_url = request.url_for("root")
-    return RedirectResponse(redirect_url, status_code=303)
+async def login_data(
+    request: Request, email: str = Form(...), password: str = Form(...)
+):
+    db = SessionLocal()
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user:
+        return templates.TemplateResponse(
+            "login.html", {"request": request, "error": "User not found"}
+        )
+
+    if not check_password_hash(user.password, password):
+        return templates.TemplateResponse(
+            "login.html", {"request": request, "error": "Incorrect password"}
+        )
+
+    SECRET_KEY = environ.get("Secret_key_chat")
+    s = Serializer(SECRET_KEY)
+    token = s.dumps({"user_id": user.id}, expires_in=timedelta(hours=1))
+
+    response = RedirectResponse(request.url_for("root"), status_code=303)
+    response.set_cookie(key="access_token", value=token, httponly=True)
+
+    return response
 
 
 # Contact
 
 
+def send_email(email_address, subject, message):
+    email_receiver = environ.get("EMAIL_RECEIVER_TODO")
+    password = environ.get("EMAIL_PASSWORD_CAFE")
+
+    email = EmailMessage()
+
+    email["From"] = email_address
+    email["To"] = email_receiver
+    email["Subject"] = subject
+    email.set_content(message)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+        smtp.login(email_receiver, password)
+        smtp.sendmail(email_address, email_receiver, email.as_string())
+
+
 @router.get("/contact", name="contact")
-async def contact_page(request: Request):
+def contact_page(request: Request):
     return templates.TemplateResponse("contact.html", {"request": request})
 
 
@@ -76,5 +177,8 @@ async def contact_data(
     subject: str = Form(...),
     message: str = Form(...),
 ):
-    redirect_url = request.url_for("root")
-    return RedirectResponse(redirect_url, status_code=303)
+    send_email(email, subject, message)
+    flash_messages = ["Your message has been sent"]
+    return templates.TemplateResponse(
+        "main_page.html", {"request": request, "flash_messages": flash_messages}
+    )
